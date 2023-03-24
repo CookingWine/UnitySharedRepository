7964ec2534ba001f08676fd17dafefdc63f5f99c
huanglekai <3181983989@qq.com>
2023-03-24 18:21:45 +0800

    no message

--- Assets/Code/TestCode/WebCookie.cs
+++ Assets/Code/TestCode/WebCookie.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -18,9 +19,38 @@ public class WebCookie :MonoBehaviour
     {
         if( Input.GetKeyDown( KeyCode.Space ) )
         {
-            StartCoroutine( RequestMusicInfo( SongsName , Length ) );
+            long time = ConvertDateTimeToUtc_13( );
+            Debug.Log( time );
+            StartCoroutine( RequestMusicInfo( ) );
         }
 
+    }
+
+    private IEnumerator RequestMusicInfo( )
+    {
+        string url = string.Empty;
+        UnityWebRequest request = new UnityWebRequest( url )
+        {
+            timeout = 5
+        };
+
+        DownloadHandlerBuffer data = new DownloadHandlerBuffer( );
+        request.downloadHandler = data;
+        yield return request.SendWebRequest( );
+        if( request.result == UnityWebRequest.Result.Success )
+        {
+            Debug.Log( $"请求到的数据为->{request.downloadHandler.text}" );
+        }
+        else
+        {
+            Debug.LogError( request.error + "请求失败" );
+        }
+
+    }
+
+    public string GetMD5( string key )
+    {
+        return key.MD5EncryptString( );
     }
 
 
@@ -48,5 +78,16 @@ public class WebCookie :MonoBehaviour
         {
             Debug.LogError( request.error + "请求失败" );
         }
+    }
+
+    /// <summary>
+    /// 时间转化为13位时间戳
+    /// </summary>
+    /// <param name="_time">获取的时间</param>
+    /// <returns></returns>
+    public static long ConvertDateTimeToUtc_13( )
+    {
+        TimeSpan timeSpan = DateTime.Now.ToUniversalTime( ) - new DateTime( 1970 , 1 , 1 , 0 , 0 , 0 , 0 );
+        return Convert.ToInt64( timeSpan.TotalMilliseconds );
     }
 }
